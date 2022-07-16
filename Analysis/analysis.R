f7b544f260ef37db23a300a9b26008becc0c1eb5
Akshaya Parthasarathy <akshaya.sarathy97@gmail.com>
2022-07-16 16:07:02 +1000

    initial code

--- Analysis/analysis.R
+++ Analysis/analysis.R
@@ -3,3 +3,21 @@
 rm(list = ls())
 
 ################ Loading Packages ################
+library(here)
+library(tidyverse) 
+library(ggplot2)
+library(janitor)
+library(dplyr)
+library(RColorBrewer)
+library(tm) # for text mining
+library(SnowballC) # for text stemming
+library(wordcloud) # word-cloud generator 
+library(syuzhet) # for sentiment analysis
+library(gridExtra)
+
+#Loading the dataset
+getwd()
+setwd("/Users/akshayaparthasarathy/Desktop/WORK/R_Text_Analysis/Dataset")
+dset <- read_csv("regrets.csv")
+
+View(dset)
